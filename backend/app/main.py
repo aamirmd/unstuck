921ab4b276d761d2f8b85972f81065d19281b865
aamirmd <aamirmd105@gmail.com>
2026-05-25 21:12:56 -0400

    Remove unnecessary file imports

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.config import settings
-from app.routers import health, strings, getprofile, chattering
+from app.routers import health, getprofile, chattering
 
 app = FastAPI(title=settings.APP_NAME)
 
@@ -15,6 +15,5 @@ app.add_middleware(
 )
 
 app.include_router(health.router)
-app.include_router(strings.router)
 app.include_router(getprofile.router)
 app.include_router(chattering.router)

71221f5ec30ea4ff141ab401c758dfc633852a9b
aamirmd <aamirmd105@gmail.com>
2026-05-26 20:08:07 -0400

    Change API base

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import { Answer, ClarityProfile, ChatMessage } from "./types";
 
 // const API_BASE = process.env.BACKEND_URL || "";
-const API_BASE = "http://localhost:8000";
+const API_BASE = "http://localhost:8000/api/v1";
 
 export async function generateProfile(
     answers: Answer[],
